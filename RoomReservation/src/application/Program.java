25701754c862c33c5ea59ac60c138a9e63235018
SrDevPereira <123840329+devsrpereira@users.noreply.github.com>
2023-05-09 22:11:59 -0300

    Very bad solution

--- RoomReservation/src/application/Program.java
+++ RoomReservation/src/application/Program.java
@@ -19,20 +19,35 @@ public class Program {
         System.out.print("Check-out date (dd/mm/yyyy): ");
         LocalDate checkout = LocalDate.parse(sc.next(),dtf1);
 
-        Reservation reservation = new Reservation(roomNumber,checkin,checkout);
+        if (!checkout.isAfter(checkin)){
+            System.out.println("Error in reservation: Check-out date must be  after  check-in date.");
+        } else {
+            Reservation reservation = new Reservation(roomNumber,checkin,checkout);
+            System.out.println(reservation);
 
-        System.out.println(reservation);
+            System.out.println();
+            System.out.println("Enter data to update the reservation:");
+            System.out.print("Check-in date (dd/mm/yyyy): ");
+            checkin = LocalDate.parse(sc.next(),dtf1);
+            System.out.print("Check-out date (dd/mm/yyyy): ");
+            checkout = LocalDate.parse(sc.next(),dtf1);
+
+            LocalDate now = LocalDate.now();
+
+            if (checkin.isBefore(now) || checkout.isBefore(now)){
+                System.out.println("Error in reservation: Reservation date for update must be future dates.");
+            } else if (!checkout.isAfter(checkin)){
+                System.out.println("Error in reservation: Check-out date must be  after  check-in date.");
+            } else {
+                reservation.updateDate(checkin,checkout);
+                System.out.println(reservation);
+            }
+        }
 
 
-        System.out.println();
-        System.out.println("Enter data to update the reservation:");
-        System.out.print("Check-in date (dd/mm/yyyy): ");
-        checkin = LocalDate.parse(sc.next(),dtf1);
-        System.out.print("Check-out date (dd/mm/yyyy): ");
-        checkout = LocalDate.parse(sc.next(),dtf1);
 
-        reservation.updateDate(checkin,checkout);
-        System.out.println(reservation);
+
+
 
 
         sc.close();
